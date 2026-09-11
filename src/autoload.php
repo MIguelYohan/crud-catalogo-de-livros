6ab55aa440701126752284c6bc413b06af500206
MIguelYohan <miguelyohan2007@gmail.com>
2026-09-11 19:50:48 -0300

    autoload aceitando referencias em codigo automaticamente

---
+++ src/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+// Carrega automaticamente os arquivos de classes e interfaces quando são referenciados no código
+
+spl_autoload_register(function (string $class) {
+    $prefix = 'App\\';
+    $baseDir = __DIR__ . '/';
+
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+
+    $relativeClass = substr($class, $len);
+    $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
+
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
